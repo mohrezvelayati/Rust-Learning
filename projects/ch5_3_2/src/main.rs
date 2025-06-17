6b90163aa32774971f1f2389f320959508b7b62e
MohammadReza Velayati <mohammadrv2003@gmail.com>
2025-06-17 14:50:54 +0330

    Chapter_5 Using Structs to Structure Related Data     - last part

--- projects/ch5_3_2/src/main.rs
+++ projects/ch5_3_2/src/main.rs
@@ -27,6 +27,6 @@ fn main() {
         height: 45,
     };
 
-    println!("Can rect1 hold rect2?", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3?", rect1.can_hold(&rect3));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
---
+++ projects/ch5_3_2/target/debug/deps/ch5_3_2.d
@@ -0,0 +1,5 @@
+F:\Programming\Rust\rust-learning\projects\ch5_3_2\target\debug\deps\ch5_3_2.exe: src\main.rs
+
+F:\Programming\Rust\rust-learning\projects\ch5_3_2\target\debug\deps\ch5_3_2.d: src\main.rs
+
+src\main.rs:

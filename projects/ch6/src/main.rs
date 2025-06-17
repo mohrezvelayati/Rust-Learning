8ac75f93e2def6e4421d40b7357ec4a34370c5c0
MohammadReza Velayati <mohammadrv2003@gmail.com>
2025-06-17 16:39:27 +0330

    Chapter 6 - Enums and Pattern Matching     - 6.1 Defining an Enum

---
+++ projects/ch6/src/main.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+
+enum Message {
+    Quit,
+    Move{x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("Message received: {:?}", self);
+    }
+
+}
+
+fn main() {
+
+    
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
---
+++ projects/ch6/target/debug/ch6.d
@@ -0,0 +1,1 @@
+F:\Programming\Rust\rust-learning\projects\ch6\target\debug\ch6.exe: F:\Programming\Rust\rust-learning\projects\ch6\src\main.rs

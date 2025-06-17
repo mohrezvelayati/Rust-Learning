dafc2629d6f103421cc9a3e652fedc08c1c6d6db
mohrezvelayati <mohammadrv2003@gmail.com>
2025-06-17 13:08:35 +0330

    Chapter_5 Using Structs to Structure Related Data     - Methos Syntax (Methods and more Parameter) : part 1

---
+++ projects/ch5_3_2/target/debug/deps/ch5_3_2-2455c0e76ff0c8ce.d
@@ -0,0 +1,5 @@
+/home/mohrez/Rust-Learning/projects/ch5_3_2/target/debug/deps/ch5_3_2-2455c0e76ff0c8ce: src/main.rs
+
+/home/mohrez/Rust-Learning/projects/ch5_3_2/target/debug/deps/ch5_3_2-2455c0e76ff0c8ce.d: src/main.rs
+
+src/main.rs:
